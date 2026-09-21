db2923a097770296a3b57ae1cee4ae2a8545ac11
leeDongju <leeapril0612@kakao.com>
2026-09-21 16:30:31 +0900

    파일업로드

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  output: "standalone",
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: "/jangPyeong",
+  assetPrefix: "/jangPyeong/",
 };
 
 export default nextConfig;
